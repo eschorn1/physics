ec604c8377be0a0c977969312bec3b3ff4eb9daa
eschorn1 <eric.schorn@nccgroup.com>
2024-05-19 17:47:41 -0500

    ch2 mid

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,7 +1,33 @@
-module Utils where
+{-# LANGUAGE FlexibleInstances #-}
 
-prettyPrint :: (Show a) => [Char] -> [([Char], a)] -> [Char]
-prettyPrint title keyValues = result
-  where
-    str = title:" ":[a ++ ": " ++ (show b) ++ ", " | (a, b) <- keyValues]
-    result = init $ init $ concat str
+module Utils (prettyPrint) where
+
+import Text.Printf
+import Vectors
+
+
+class PP r where
+    pp :: String -> r
+
+instance PP String where
+    pp = id
+
+instance (PP r) => PP (Char -> r) where
+    pp s c = pp (s ++ [c])
+
+instance (PP r) => PP (Integer -> r) where
+    pp s c = pp (s ++ (show c))
+
+instance (PP r) => PP (Double -> r) where
+    pp s c = pp (s ++ (printf "%+.3e" c))
+
+instance (PP r) => PP (Vec -> r) where
+    pp s c = pp (s ++ (show c))
+
+instance (PP r) => PP (String -> r) where
+    pp s c = pp (s ++ c)
+
+
+prettyPrint :: (PP r) => r
+prettyPrint = pp ""
+
--- src/Vectors.hs
+++ src/Vectors.hs
@@ -1,49 +1,66 @@
+{-# OPTIONS_GHC -Wno-partial-fields -Wno-unused-top-binds #-} -- TODO latter should go away
+
 module Vectors ((^+^), (^-^), (*^), (^*), (^/), (<.>), (><),
-  angle, angle2D, degrees2Radians, iHat, jHat, kHat, magnitude, polarCoord, radians2Degrees,
+  angle, angleXY, degrees2Radians, iHat, jHat, kHat, magnitude, polarCoord, radians2Degrees,
   Vec, vec, vec2D, xComp, yComp, zComp, zeroV) where
 
+import Text.Printf
+
 type R = Double
+type Rads = Double
 
-infixl 6 ^+^
-(^+^) :: Vec -> Vec -> Vec
-Vec ax ay az ^+^ Vec bx by bz = Vec (ax+bx) (ay+by) (az+bz)
 
-infixl 6 ^-^
-(^-^) :: Vec -> Vec -> Vec
-Vec ax ay az ^-^ Vec bx by bz = Vec (ax-bx) (ay-by) (az-bz)
-
-infixl 7 *^
-(*^) :: R -> Vec -> Vec
-c *^ Vec ax ay az = Vec (c*ax) (c*ay) (c*az)
-
-infixl 7 ^*
-(^*) :: Vec -> R -> Vec
-Vec ax ay az ^* c = Vec (c*ax) (c*ay) (c*az)
-
-infixr 7 ^/
-(^/) :: Vec -> R -> Vec
-Vec ax ay az ^/ c = Vec (ax/c) (ay/c) (az/c)
-
-infixr 7 <.>
-(<.>) :: Vec -> Vec -> R
-Vec ax ay az <.> Vec bx by bz = ax*bx + ay*by + az*bz
-
-infixl 7 ><
-(><) :: Vec -> Vec -> Vec
-Vec ax ay az >< Vec bx by bz = Vec (ay*bz - az*by) (az*bx - ax*bz) (ax*by - ay*bx)
-
-data Vec = Vec { xComp :: R, yComp :: R, zComp :: R} deriving (Eq)
+data Vec = Cart  { xComp :: R, yComp :: R, zComp :: R}
+         | Polar { val1a :: R, val2a :: Rads, val3a :: Rads} deriving (Eq)
 
 instance Show Vec where
-  show (Vec x y z) = "vec " ++ showDouble x ++ " " ++ showDouble y ++ " " ++ showDouble z
+  show (Cart x y z) = "vec " ++ showDouble x ++ " " ++ showDouble y ++ " " ++ showDouble z
+  show (Polar _ _ _) = undefined
 
 showDouble :: R -> String
 showDouble x
-  | x < 0       = "(" ++ show x ++ ")"
-  | otherwise   = show x
+  | x < 0       = "(" ++ printf "%+.3e" x ++ ")"
+  | otherwise   = printf "%+.3e" x
 
 vec :: R -> R -> R -> Vec
-vec = Vec
+vec = Cart
+
+
+infixl 6 ^+^
+(^+^) :: Vec -> Vec -> Vec
+Cart ax ay az ^+^ Cart bx by bz = Cart (ax+bx) (ay+by) (az+bz)
+_ ^+^ _ = undefined
+
+infixl 6 ^-^
+(^-^) :: Vec -> Vec -> Vec
+Cart ax ay az ^-^ Cart bx by bz = Cart (ax-bx) (ay-by) (az-bz)
+_ ^-^ _ = undefined
+
+infixl 7 *^
+(*^) :: R -> Vec -> Vec
+c *^ Cart ax ay az = Cart (c*ax) (c*ay) (c*az)
+_ *^ _ = undefined
+
+infixl 7 ^*
+(^*) :: Vec -> R -> Vec
+Cart ax ay az ^* c = Cart (c*ax) (c*ay) (c*az)
+_ ^* _ = undefined
+
+infixr 7 ^/
+(^/) :: Vec -> R -> Vec
+Cart ax ay az ^/ c = Cart (ax/c) (ay/c) (az/c)
+_ ^/ _ = undefined
+
+infixr 7 <.>
+(<.>) :: Vec -> Vec -> R
+Cart ax ay az <.> Cart bx by bz = ax*bx + ay*by + az*bz
+_ <.> _ = undefined
+
+infixl 7 ><
+(><) :: Vec -> Vec -> Vec
+Cart ax ay az >< Cart bx by bz = Cart (ay*bz - az*by) (az*bx - ax*bz) (ax*by - ay*bx)
+_ >< _ = undefined
+
 
 vec2D :: R -> R -> Vec
 vec2D r degrees = vec (r * cos (degrees2Radians degrees)) (r * sin (degrees2Radians degrees)) 0
@@ -63,17 +80,17 @@ zeroV = vec 0 0 0
 magnitude :: Vec -> R
 magnitude v = sqrt $ (xComp v)^(2 :: Integer) + (yComp v)^(2 :: Integer) + (zComp v)^(2 :: Integer)
 
-angle2D :: Vec -> R
-angle2D v = result
+angleXY :: Vec -> R
+angleXY v = result
   where
     res = radians2Degrees $ atan2 (yComp v) (xComp v)  -- note: atan2
     result = if res < 0 then res + 360 else res        -- maybe put this in conversion fns?
 
-angle :: Vec -> Vec -> R  -- in radians
+angle :: Vec -> Vec -> R  -- in radians; suspect
 angle a b = acos ((a <.> b) / (magnitude a * magnitude b))
 
 polarCoord :: Vec -> (R, R)
-polarCoord v = (magnitude v, angle2D v)
+polarCoord v = (magnitude v, angleXY v)
 
 radians2Degrees :: R -> R
 radians2Degrees r = (r * 360) / (2 * pi)
