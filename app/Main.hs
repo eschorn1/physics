74d844e99f9c9dcbac3169e91ca5ab0730e1a364
eschorn1 <eric.schorn@nccgroup.com>
2024-07-17 13:46:52 -0500

    to cc3

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,30 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
 module Main (main) where
 
+-- stack repl
+-- :r
+
 import Ch1
+import Vectors
 
 main :: IO ()
 main = do
   putStrLn $ "\n\n" ++ ch1
+
+
+quadratic :: Double -> Double -> Double -> (Double, Double)
+quadratic a b c = (res1, res2)
+  where
+    det = sqrt (b * b - 4 * a * c)
+    res1 = ((-b) + det) / (2 * a)
+    res2 = ((-b) - det) / (2 * a)
+
+norm2 :: Double -> Double -> Double
+norm2 x y = sqrt (x * x + y * y)
+
+cosDeg :: Double -> Double
+cosDeg = cos . degrees2Radians
+
+sinDeg :: Double -> Double
+sinDeg = sin . degrees2Radians
