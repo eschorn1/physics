364e14bc1923b9d58edf4c7dc8be9f03d462961d
eschorn1 <eschorn@integritychain.com>
2024-08-16 21:04:04 -0500

    eqns

---
+++ src/Equations.hs
@@ -0,0 +1,15 @@
+module Equations where
+
+
+{-#
+
+To implement....
+
+  (8.2 p184)  ΔK + ΔU + ΔE_int = W + Q + T_MW + T_MT + T_ET + T_ER
+  where
+    K     -> kinetic energy         T_MW -> mechanical waves
+    U     -> potential energy       T_MT -> matter transfer
+    E_int -> internal energy        T_ET -> electrical transmission
+    W     -> work                   T_ER -> electromagnetic radiation
+    Q     -> heat transfer
+#-}
